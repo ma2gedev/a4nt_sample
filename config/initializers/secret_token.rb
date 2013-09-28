11ba3c69228b6232c1877b100797a8df86f2ba2b
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-09-28 22:54:35 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+A4ntSample::Application.config.secret_key_base = '16dd2dde8d78b4f834e64500b8aae388c618b99706ad5ceed2593831f5d3477817c98831d9936a5be71228a47b57b1c2ba37244b8d6f5d6c176df59850d5c74d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+A4ntSample::Application.config.session_store :cookie_store, key: '_a4nt_sample_session'
